153eaeaa5eb4f7951fb38b325d9a8fd5fc19bb4f
Ruari Day <ruariday@gmail.com>
2020-03-16 13:05:28 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Dog # define class
+  def name=(dogs_name) #pass in argument
+    @current_dogs_name = dogs_name #declare intance variable with @ symbol
+  end 
+  def name
+    @current_dogs_name #call the instance variable 
+  end 
+  def bark #define bark function 
+    puts "woof!" #make the dog talk
+  end 
+end 
